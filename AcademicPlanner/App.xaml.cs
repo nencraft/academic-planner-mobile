cd1773f5a049128ed9db9eda34cc83bb3638ee83
Raffi G <raffi.gevorg@gmail.com>
2026-04-18 14:49:50 -0700

    feat: add pin fields and authentication support

--- AcademicPlanner/App.xaml.cs
+++ AcademicPlanner/App.xaml.cs
@@ -1,16 +1,33 @@
 ﻿using AcademicPlanner.Services;
+using AcademicPlanner.Views;
 
 namespace AcademicPlanner;
 
 public partial class App : Application
 {
-    public App()
+    private readonly AuthenticationService _authService;
+
+    public App(AuthenticationService authService)
     {
         InitializeComponent();
+        _authService = authService;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AppShell());
+        Page startupPage = new ContentPage();
+
+        var window = new Window(startupPage);
+
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            bool hasAccount = await _authService.HasAccountAsync();
+
+            window.Page = hasAccount
+                ? new LoginPage(_authService)
+                : new SetupAccountPage(_authService);
+        });
+
+        return window;
     }
 }

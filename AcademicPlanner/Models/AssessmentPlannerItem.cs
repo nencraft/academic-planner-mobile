b0073ac27050d7c7ca793061f4f036666ec0beb9
Raffi G <raffi.gevorg@gmail.com>
2026-04-19 13:06:58 -0700

    feat: add PlannerItem inheritance layer

---
+++ AcademicPlanner/Models/AssessmentPlannerItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicPlanner.Models;
+
+public class AssessmentPlannerItem : PlannerItem
+{
+    public string AssessmentType { get; set; } = string.Empty;
+
+    public string ParentCourseTitle { get; set; } = string.Empty;
+
+    public override string ItemType => "Assessment";
+
+    public override string Subtitle => $"{AssessmentType} | Course: {ParentCourseTitle}";
+
+    public override string NavigationRoute => $"{nameof(Views.AssessmentEditPage)}?assessmentId={SourceId}";
+
+    public override ReportRow ToReportRow()
+    {
+        return new ReportRow
+        {
+            ItemType = ItemType,
+            Title = Title,
+            Parent = ParentCourseTitle,
+            StartDate = StartDate.ToString("MM/dd/yyyy"),
+            EndDate = EndDate.ToString("MM/dd/yyyy"),
+            Status = AssessmentType
+        };
+    }
+}
---
+++ AcademicPlanner/Models/TermPlannerItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicPlanner.Models;
+
+public class TermPlannerItem : PlannerItem
+{
+    public override string ItemType => "Term";
+
+    public override string Subtitle => $"Term dates: {StartDate:MM/dd/yyyy} - {EndDate:MM/dd/yyyy}";
+
+    public override string NavigationRoute => $"{nameof(Views.TermOverviewPage)}?termId={SourceId}";
+}
